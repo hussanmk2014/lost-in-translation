3b06b66ac28840b7b443f2b3c52abb970ebbd55c
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-01-29 15:56:38 +0100

    style
    Changes of style in the code

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -6,8 +6,7 @@ import classes from "./ProfileHeader.module.css";
 const ProfileHeader = ({username}) => {
     return (
         <section className={classes.ProfileHeader}>
-            <h4>Welcome, <span className={classes.profileUsername}>{username || "Stranger"}</span> !</h4>
-            <h4>in your personal page</h4>
+            <h4>Welcome <span className={classes.profileUsername}>{username || "Stranger"}</span> in your personal page!</h4>
         </section>
     );
 };
